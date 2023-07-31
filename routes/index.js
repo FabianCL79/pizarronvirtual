f919aea98ad2be0ede3b1930438cf249e41dbe80
FabianCL79 <cervantesfabian.7.9@gmail.com>
2023-07-31 11:28:07 -0600

    Initial commit

---
+++ routes/index.js
@@ -0,0 +1,34 @@
+module.exports = function (io) {
+
+  var express = require('express');
+  var router = express.Router();
+
+
+  const history = [];
+
+
+  router.get('/', function (req, res, next) {
+    res.render('index', { title: 'App' });
+  });
+
+  /* Socket.io listeners */
+
+  io.on('connection', (socket) => {
+    console.log('A user connected');
+
+
+    console.log('Syncing new user"s canvas from history')
+    for (let item of history)
+      socket.emit('update_canvas', item);
+
+
+    socket.on('update_canvas', function (data) {
+
+      history.push(data);
+
+      socket.broadcast.emit('update_canvas', data);
+    });
+  })
+
+  return router;
+}
